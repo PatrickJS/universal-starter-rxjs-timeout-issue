45efe7287ea4f0555cd9ee4f3e405387e6319e4f
PatrickJS <github@gdi2290.com>
2016-11-15 11:53:44 -0500

    init

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/timeout';
+
+import { CacheService  } from './cache.service';
+
+
+@Injectable()
+export class ApiService {
+  constructor(public _http: Http) {
+
+  }
+
+ /**
+  * whatever domain/feature method name
+  */
+  get(url: string, options?: any) {
+    return this._http.get(url, options)
+      .timeout(2000)
+      .map(res => res.json())
+      .catch(err => {
+        console.log('Error: ', err);
+        return Observable.throw(err);
+      });
+  }
+
+}
